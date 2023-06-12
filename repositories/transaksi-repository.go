10cb75b480b10d1dd951192db0ea2730b40af0f4
Merylp2 <merylputrapratama06@gmail.com>
2023-06-12 15:33:29 +0700

    update transaction repository

--- repositories/transaksi-repository.go
+++ repositories/transaksi-repository.go
@@ -20,36 +20,88 @@ type transactionRepository struct {
 
 // Save transaction product
 func (r *transactionRepository) SaveTransactionProduct(transactionProduct models.TransactionProduct) error {
-	return r.DB.Create(transactionProduct).Error
-}
-
-// Delete transaction product
-func (r *transactionRepository) DeleteTransactionProduct(transactionProduct models.TransactionProduct) error {
-	return r.DB.Delete(transactionProduct).Error
+	// Create a new transaction product record in the database
+    err := r.DB.Create(&transactionProduct).Error
+    if err != nil {
+        return err
+    }
+    // Return a success response
+    return nil
 }
 
 // Get transaction product
-func (r *transactionRepository) GetTransactionProducts(customerID uint64) ([]*models.TransactionProduct, error) {
-	var transactionProduct []*models.TransactionProduct
-	if err := r.DB.Where("customer_id = ?", customerID).Find(&transactionProduct).Error; err != nil {
-		return nil, err
-	}
-	return transactionProduct, nil
+func (r *transactionRepository) GetTransactionProduct(productID string) (*models.TransactionProduct, error) {
+    // Get the transaction product record from the database
+    transactionProduct := &models.TransactionProduct{}
+    err := r.DB.Where("product_id = ?", productID).First(&transactionProduct).Error
+    if err != nil {
+        return nil, err
+    }
+    // Return the transaction product
+    return transactionProduct, nil
+}
+
+// Delete transaction product
+func (r *transactionRepository) DeleteTransactionProduct(productID string) error {
+    // Delete the transaction product record from the database
+    err := r.DB.Where("product_id = ?", productID).Delete(&models.TransactionProduct{}).Error
+    if err != nil {
+        return err
+    }
+    // Return a success response
+    return nil
 }
 
 // Save transaction ticket
-func (r *transactionRepository) SaveTransationTicket(ticket models.Ticket) error {
-	return r.DB.Create(ticket).Error
+func (r *transactionRepository) SaveTransactionTicket(transactionTicket *models.Ticket) error {
+    // Create a new transaction ticket record in the database
+    err := r.DB.Create(&transactionTicket).Error
+    if err != nil {
+        return err
+    }
+    // Return a success response
+    return nil
 }
 
-// Get transaction ticket
-func (r *transactionRepository) GetTransactionTicket(customerID uint64) ([]*models.Ticket, error) {
-	var ticket []*models.Ticket
-	if err := r.DB.Where("customer_id = ?", customerID).Find(&ticket).Error; err != nil {
-		return nil, err
-	}
-	return ticket, nil
+func (r *transactionRepository) GetTransactionTicket(ticketID string) (*models.Ticket, error) {
+    // Get the transaction ticket record from the database
+    transactionTicket := &models.Ticket{}
+    err := r.DB.Where("ticket_id = ?", ticketID).First(&transactionTicket).Error
+    if err != nil {
+        return nil, err
+    }
+    // Return the transaction ticket
+    return transactionTicket, nil
 }
+
+
+// // Delete transaction product
+// func (r *transactionRepository) DeleteTransactionProduct(transactionProduct models.TransactionProduct) error {
+// 	return r.DB.Delete(transactionProduct).Error
+// }
+
+// // Get transaction product
+// func (r *transactionRepository) GetTransactionProducts(customerID uint64) ([]*models.TransactionProduct, error) {
+// 	var transactionProduct []*models.TransactionProduct
+// 	if err := r.DB.Where("customer_id = ?", customerID).Find(&transactionProduct).Error; err != nil {
+// 		return nil, err
+// 	}
+// 	return transactionProduct, nil
+// }
+
+// // Save transaction ticket
+// func (r *transactionRepository) SaveTransationTicket(ticket models.Ticket) error {
+// 	return r.DB.Create(ticket).Error
+// }
+
+// // Get transaction ticket
+// func (r *transactionRepository) GetTransactionTicket(customerID uint64) ([]*models.Ticket, error) {
+// 	var ticket []*models.Ticket
+// 	if err := r.DB.Where("customer_id = ?", customerID).Find(&ticket).Error; err != nil {
+// 		return nil, err
+// 	}
+// 	return ticket, nil
+// }
 
 // GetTransaksisRepository() ([]*models.Transaksi, error)
 // GetTransaksiRepository(id string) (*models.Transaksi, error)
